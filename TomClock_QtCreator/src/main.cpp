8b0add2063f342d9845d78ac197781b8c1ed35b1
Pologue <412295017@qq.com>
2023-06-28 23:59:55 +0800

    尝试连接各模块

--- TomClock_QtCreator/src/main.cpp
+++ TomClock_QtCreator/src/main.cpp
@@ -1,13 +1,4 @@
-#include "mainwindow.h"
-#include "achievementwindow.h"
-#include "createmissionwindow.h"
-#include "historywindow.h"
-#include "missionlistwindow.h"
-#include "runwindow.h"
-#include "settings.h"
-#include "about.h"
-#include "ourteam.h"
-#include "tomclockdatabase.h"
+#include "tomclock.h"
 
 #include <QApplication>
 #include <QLocale>
@@ -26,66 +17,44 @@ int main(int argc, char *argv[])
             break;
         }
     }
-    //先创建所有窗口，跳转页面就是隐藏和显示不同的窗口
-    MainWindow mainW;
-    AchievementWindow achievementW;
-    CreateMissionWindow createW;
-    HistoryWindow historyW;
-    MissionListWindow listW;
-    RunWindow runW;
-    Settings settingW;
-    about aboutW;
-    OurTeam teamW;
 
-    //test
-    TomClockDatabase tcdb;
-    Mission m1(1, "study math", QTime().fromString("00:25", "mm:ss"), QTime().fromString("00:05", "mm:ss"), QDate::currentDate());
-    Mission m2(2, "study english", QTime().fromString("00:25", "mm:ss"), QTime().fromString("00:05", "mm:ss"), QDate::currentDate());
-    QVector<Mission> ml = {m1, m2};
-    QVector<Mission> qm;
-    History h1(1, QDate::currentDate(), "study math", 0, QTime().fromString("11:45", "mm:ss"));
-    QVector<History> qh = {h1};
-    Achievement a1("First Tomato", 0, "Get your first tomato by complete a mission.");
-    QVector<Achievement> al = {a1};
-    QVector<Achievement> qa;
-    tcdb.createMission(m1);
-    tcdb.createMission(m2);
-    tcdb.deleteMission(1);
-    tcdb.updateMission(ml);
-    qm = tcdb.queryMission();
-//    qDebug(QString("%1").arg(qm[0].getId()).toLatin1());
-//    qDebug(QString("%1").arg(qm[1].getId()).toLatin1());
-    tcdb.addHistory(h1);
-    qh = tcdb.queryHistory();
-//    qDebug(QString("%1").arg(qh[0].getId()).toLatin1());
-    tcdb.initAchievement(al);
-    qa = tcdb.queryAchievement();
-//    qDebug(QString("%1").arg(qa[0].getState()).toLatin1());
-    tcdb.updateAchievement(a1);
-    qa = tcdb.queryAchievement();
-//    qDebug(QString("%1").arg(qa[0].getState()).toLatin1());
-    //test end
-    mainW.show();
+    TomClock tomClock;
+    tomClock.initAchieveWindow();
+    tomClock.initHistoryWindow();
+    tomClock.initMissionListWindow();
+    tomClock.showWindow(); //显示主窗口
 
-    //各页面之间进行跳转的实现
-    QObject::connect(&mainW, SIGNAL(JumptoAchievement()), &achievementW, SLOT(MaintoAchievement()));
-    QObject::connect(&mainW, SIGNAL(JumptoHistory()), &historyW, SLOT(MaintoHistory()));
-   // QObject::connect(&mainW, SIGNAL(JumptoMissionCreate()), &createW, SLOT(MaintoCreate()));
-    QObject::connect(&mainW, SIGNAL(JumptoMissionList()), &listW, SLOT(MaintoList()));
-    QObject::connect(&mainW, SIGNAL(JumptoSettings()), &settingW, SLOT(MaintoSettings()));
-    QObject::connect(&mainW, SIGNAL(JumptoAbout()), &aboutW, SLOT(MaintoAbout()));
-    QObject::connect(&settingW, SIGNAL(BacktoMain()), &mainW, SLOT(SettingstoMain()));
-    QObject::connect(&historyW, SIGNAL(BacktoMain()), &mainW, SLOT(HistorytoMain()));
-    QObject::connect(&aboutW, SIGNAL(AbouttoTeam()), &teamW, SLOT(AbouttoTeam()));
-    QObject::connect(&aboutW, SIGNAL(BacktoMain()), &mainW, SLOT(AbouttoMain()));
-    QObject::connect(&teamW, SIGNAL(BacktoAbout()), &aboutW, SLOT(TeamtoAbout()));
-    QObject::connect(&listW,SIGNAL(jumpToRunWindows(Mission)),&runW,SLOT(ListtoRun(Mission)));
-    //QObject::connect(&createW,SIGNAL(sentAndJump(Mission)),&mainW,SLOT(othertoMain()));
-    QObject::connect(&createW,SIGNAL(sentAndJump(Mission)),&listW,SLOT(recieveMission(Mission)));
-    QObject::connect(&listW,SIGNAL(create()),&createW,SLOT(toCreate()));
-    QObject::connect(&runW, SIGNAL(JumptoMain()), &mainW, SLOT(RuntoMain()));
-    QObject::connect(&achievementW, SIGNAL(JumptoMain()), &mainW, SLOT(AchievetoMain()));
-    QObject::connect(&listW, SIGNAL(BacktoMain()), &mainW, SLOT(ListtoMain()));
+//    //test
+//    TomClockDatabase tcdb;
+//    Mission m1(1, "study math", QTime().fromString("00:25", "mm:ss"), QTime().fromString("00:05", "mm:ss"), QDate::currentDate());
+//    Mission m2(2, "study english", QTime().fromString("00:25", "mm:ss"), QTime().fromString("00:05", "mm:ss"), QDate::currentDate());
+//    QVector<Mission> ml = {m1, m2};
+//    QVector<Mission> qm;
+//    History h1(1, QDate::currentDate(), "study math", 0, QTime().fromString("11:45", "mm:ss"));
+//    QVector<History> qh = {h1};
+//    Achievement a1("First Tomato", 0, "Get your first tomato by complete a mission.");
+//    QVector<Achievement> al = {a1};
+//    QVector<Achievement> qa;
+//    tcdb.createMission(m1);
+//    tcdb.createMission(m2);
+//    tcdb.deleteMission(1);
+//    tcdb.updateMission(ml);
+//    qm = tcdb.queryMission();
+////    qDebug(QString("%1").arg(qm[0].getId()).toLatin1());
+////    qDebug(QString("%1").arg(qm[1].getId()).toLatin1());
+//    tcdb.addHistory(h1);
+//    qh = tcdb.queryHistory();
+////    qDebug(QString("%1").arg(qh[0].getId()).toLatin1());
+//    tcdb.initAchievement(al);
+//    qa = tcdb.queryAchievement();
+////    qDebug(QString("%1").arg(qa[0].getState()).toLatin1());
+//    tcdb.updateAchievement(a1);
+//    qa = tcdb.queryAchievement();
+////    qDebug(QString("%1").arg(qa[0].getState()).toLatin1());
+//    //test end
+//    mainW.show();
+
+
     //我尝试了在各个页面的cpp文件下写页面跳转，但是好像先创建所有窗口方法的跳转不支持，你们有解决办法可以重写一下
 
     return a.exec();
--- TomClock_QtCreator/src/tomclock.cpp
+++ TomClock_QtCreator/src/tomclock.cpp
@@ -3,4 +3,62 @@
 TomClock::TomClock()
 {
     tcDatabase = new TomClockDatabase();
+    mainW = new MainWindow();
+    achievementW = new AchievementWindow();
+    createW = new CreateMissionWindow();
+    historyW = new HistoryWindow();
+    missionListW = new MissionListWindow();
+    runW = new RunWindow();
+    settingW = new Settings();
+    aboutW = new about();
+    teamW = new OurTeam();
+
+    //各页面之间进行跳转的实现
+    connect(mainW, SIGNAL(JumptoAchievement()), achievementW, SLOT(MaintoAchievement()));
+    connect(mainW, SIGNAL(JumptoHistory()), historyW, SLOT(MaintoHistory()));
+//    connect(&mainW, SIGNAL(JumptoMissionCreate()), &createW, SLOT(MaintoCreate()));
+    connect(mainW, SIGNAL(JumptoMissionList()), missionListW, SLOT(MaintoList()));
+    connect(mainW, SIGNAL(JumptoSettings()), settingW, SLOT(MaintoSettings()));
+    connect(mainW, SIGNAL(JumptoAbout()), aboutW, SLOT(MaintoAbout()));
+    connect(settingW, SIGNAL(BacktoMain()), mainW, SLOT(SettingstoMain()));
+    connect(historyW, SIGNAL(BacktoMain()), mainW, SLOT(HistorytoMain()));
+    connect(aboutW, SIGNAL(AbouttoTeam()), teamW, SLOT(AbouttoTeam()));
+    connect(aboutW, SIGNAL(BacktoMain()), mainW, SLOT(AbouttoMain()));
+    connect(teamW, SIGNAL(BacktoAbout()), aboutW, SLOT(TeamtoAbout()));
+    connect(missionListW,SIGNAL(jumpToRunWindows(Mission)),runW,SLOT(ListtoRun(Mission)));
+//    connect(&createW,SIGNAL(sentAndJump(Mission)),&mainW,SLOT(othertoMain()));
+    connect(createW,SIGNAL(sentAndJump(Mission)),missionListW,SLOT(recieveMission(Mission)));
+    connect(missionListW,SIGNAL(create()),createW,SLOT(toCreate()));
+    connect(runW, SIGNAL(JumptoMain()), mainW, SLOT(RuntoMain()));
+    connect(achievementW, SIGNAL(JumptoMain()), mainW, SLOT(AchievetoMain()));
+    connect(missionListW, SIGNAL(BacktoMain()), mainW, SLOT(ListtoMain()));
+}
+
+TomClock::~TomClock()
+{
+    delete tcDatabase;
+    delete mainW;
+    delete achievementW;
+    delete createW;
+    delete historyW;
+    delete missionListW;
+    delete runW;
+    delete settingW;
+    delete aboutW;
+    delete teamW;
+}
+
+void TomClock::initAchieveWindow()
+{
+
+}
+
+void TomClock::initMissionListWindow()
+{
+
+}
+
+void TomClock::initHistoryWindow()
+{
+
 }
