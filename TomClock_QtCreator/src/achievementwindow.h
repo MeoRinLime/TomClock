941e79237065df3433ebb2bca4d93393d421e6b8
MeoRinLime <lime_omie@whu.edu.cn>
2023-06-28 15:30:28 +0800

    成就页面的更新

--- TomClock_QtCreator/src/achievementwindow.h
+++ TomClock_QtCreator/src/achievementwindow.h
@@ -19,16 +19,12 @@ public:
     void changeTomatoNum(int tomatoNum);                                //改变番茄数
     void updateAchievementState(int indexOfAchievement, bool newState); //更新成就的状态
 
-signals:
-    void JumptoMain();
-
 private:
     Ui::AchievementWindow *ui;
     //实现自适应窗口大小
     QList<QWidget*> allChildWidgets;       //储存所有子控件
     QMap<QWidget*, QRect> allWidgetRect;   //保存所有子控件的初始大小
     void resizeEvent(QResizeEvent *event); //重写resizeEvent
-    void closeEvent(QCloseEvent *event);   //重写closeEvent，跳转回mainWindow
 
 private slots:
     void MaintoAchievement();
